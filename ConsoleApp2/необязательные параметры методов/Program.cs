09aa27f3a17b9d2f5e709de095f9b6d9aac627a0
Bahodur Tursunov <tursunovb18@gmail.com>
2023-08-21 11:29:05 +0500

    необязательные параметры методов

---
+++ ConsoleApp2/необязательные параметры методов/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace необязательные_параметры_методов
+{
+    internal class Program
+    {
+        static int Sum(int a, int b, bool enableLogging = false)
+        {
+            int result = a + b;
+            if (enableLogging)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                Console.WriteLine(result);
+            }
+            return result; 
+        }
+        static void Main(string[] args)
+        {
+            int result = Sum(5, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
