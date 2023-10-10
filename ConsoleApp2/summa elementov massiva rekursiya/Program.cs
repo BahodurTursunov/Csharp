f477d63f07d0c75ba7c09713c5a8ea1fbda3029d
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-10 09:38:31 +0500

    added new projects

--- ConsoleApp2/summa elementov massiva rekursiya/Program.cs
+++ ConsoleApp2/summa elementov massiva rekursiya/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace summa_elementov_massiva_rekursiya
 {
+    // найти сумму элементов массива с помощью рекурсии.
+
     internal class Program
     {
+        static int Sum(int[] myArray, int i = 0)
+        {
+            if (i >= myArray.Length) return 0;
+
+            return myArray[i] + Sum(myArray, i+1);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] myArray = { 5, 4, 7 };
+            int result = Sum(myArray);
         }
     }
 }
