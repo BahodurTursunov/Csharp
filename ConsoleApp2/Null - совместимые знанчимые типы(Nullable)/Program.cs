0b9c935ec47cadcc4833753d597e8474b4a89449
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-10 10:10:07 +0500

    nullable types

---
+++ ConsoleApp2/Null - совместимые знанчимые типы(Nullable)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Null___совместимые_знанчимые_типы_Nullable_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
