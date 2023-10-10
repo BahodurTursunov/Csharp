b2987d96f225123a523eb19171945b3cc737c342
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-10 10:45:32 +0500

    nullable

--- ConsoleApp2/Null - совместимые знанчимые типы(Nullable)/Program.cs
+++ ConsoleApp2/Null - совместимые знанчимые типы(Nullable)/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int? i = 2;
+            //Console.WriteLine(i); 
+            //Console.WriteLine(i==null);
+            //Console.WriteLine(i.HasValue);
+            //Console.WriteLine(i.GetValueOrDefault());
+            //Console.WriteLine(i.GetValueOrDefault(3));
+            //Console.WriteLine(i ?? 55);
+            //Console.WriteLine(i.Value);
+
+            int? a = 5;
+            int? b = 22;
+            int? result = a + b;
+            Console.WriteLine(result);
+            Console.WriteLine(a==b);
+            Console.WriteLine(a>b);
+            Console.WriteLine(a<b);
         }
     }
 }
