a14c3e8750186b9d57b156e2e0723c1bfe352ea3
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-11 09:04:09 +0500

    enum

---
+++ ConsoleApp2/enum-что это и для чего это нужно/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_что_это_и_для_чего_это_нужно
+{
+    internal class Class1
+    {
+        enum DayOfWeek
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            DayOfWeek dayOfWeek;
+            int value = 5;
+            if (Enum.IsDefined(typeof(DayOfWeek), value))
+            {
+                dayOfWeek = (DayOfWeek)value; // приводим dayOfWeek к int
+            }
+            else
+            {
+                throw new Exception("Invalid DayOfWeek value.");
+            }
+            Console.WriteLine(dayOfWeek);
+        }
+    }
+    
+}
---
+++ ConsoleApp2/enum-что это и для чего это нужно/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace enum_что_это_и_для_чего_это_нужно
+{
+    internal class Program
+    {
+        // enum (перечисления) 
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+                int keyCode = (int)key;
+                Console.WriteLine($"\tEnum {key}\tKey Code {keyCode}");
+                if(key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Вы нажали Enter!");
+                }
+            }
+
+        }
+    }
+}
