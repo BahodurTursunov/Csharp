b03e1aabb160b3f1ac5dbbc04acfcaf822c8edfd
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-15 19:40:33 +0500

    локальные функции

---
+++ ConsoleApp2/Локальные функции/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Локальные_функции
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Compare(int[] numbers1, int[] numbers2)
+            {
+                int numbres1Sum = 0;
+                int numbres2Sum = 0;
+
+                //foreach (var numbers in numbers1)
+                //{
+                //    numbres1Sum += numbers;
+                //}
+
+                //foreach (var numbers in numbers2)
+                //{
+                //    numbres2Sum += numbers;
+                //}
+
+                int Sum(int[] numbers)
+                {
+                    int res = 0;
+                    foreach (var item in numbers)
+                    {
+                        res += item;
+                    }
+                    return res;
+                }
+
+                if (numbres1Sum > numbres2Sum)
+                {
+                    Console.WriteLine("Сумма чисел nuum1 >");
+                }
+                else if (numbres1Sum < numbres2Sum)
+                {
+                    Console.WriteLine("Сумма чисел nuum1 <");
+                }
+                else
+                {
+                    Console.WriteLine("сумма чисел =");
+                }
+            }
+            int[] num = { 1, 2, 3 };
+            int[] num2 = { 1, 2, 3 };
+            Compare(num, num2);
+        }
+    }
+}
---
+++ ConsoleApp2/Модификаторы доступа/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Xml.Serialization;
+
+namespace Модификаторы_доступа
+{
+    // модификаторы доступа public private, для членов класса
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //void sum(int intial, params int[] numbers)
+            //{
+            //    int result = intial;
+            //    foreach (var item in numbers)
+            //    {
+            //        result += item;
+            //    }
+            //    Console.WriteLine(result);
+            //}
+            //int[] nums = {1,2,3,4,5,6,7};
+            //sum(10,nums);
+            //sum(1, 2, 3, 4);
+            //sum(1, 2, 3);
+            //sum(20);
+
+            //static void display(int[] numbers)
+            //{
+            //    int res = 0;
+            //    for (int i = 0; i < numbers.Length; i++)
+            //    {
+            //        res+= numbers[i];
+            //    }
+            //    Console.WriteLine(res);
+            //}
+            //display(new int[] {1,2,3,4});
+
+
+            
+            Display(1, 3, 4, 2);
+            Console.ReadKey();
+            
+
+            static void Display(params int[] numbers, int r)
+            {
+                int result = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    result += numbers[i];
+                }
+                result *= r;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
