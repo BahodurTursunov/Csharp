06d1078febdb9a9279e4114b0de7e0e9fd4d3259
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-23 19:47:06 +0500

    перегрузка конструкторов класса

---
+++ ConsoleApp2/overloading class constructors(перегрузка конструкторов класса)/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace overloading_class_constructors_перегрузка_конструкторов_класса_
+{
+    class Point
+    {
+        public Point()
+        {
+            _x = _y = 1;
+        }
+
+        public Point(int x, int y)
+        {
+            this._x = x;
+            this._y = y;
+        }
+
+        private int _x;
+        private int _y;
+
+        public void Print()
+        {
+            Console.WriteLine($"{_x} - {_y}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.Print();
+        }
+    }
+}
