374156a198fb38e0e7ab58a794732ec1ac4f36dd
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-24 18:48:42 +0500

    Properties

---
+++ ConsoleApp2/Properties(свойства автоматические свойства)/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Point
+{
+    private int x;
+
+    public void SetX(int x)
+    {
+        this.x = x;
+    }
+
+    public int GetX()
+    {
+        return x;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point p = new Point();
+        p.SetX(10);
+        
+        int x = p.GetX();
+        Console.WriteLine(x);
+    }
+}
