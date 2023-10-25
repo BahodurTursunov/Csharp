aabf2334c04e31057031dbb3c36495ddf32cc845
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-25 14:03:37 +0500

    static methods C#

---
+++ ConsoleApp2/static methods C#/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Ключевое слово static
+ * Статические методы класса
+ * Статические свойства класса
+ */
+
+class MyClass
+{
+	private static int counter;
+
+    public MyClass()
+    {
+        counter++;
+    }
+    public static int Counter
+	{
+		get { return counter; }
+		set { counter = value; }
+	}
+
+}
+class Programm
+{
+    static void Main(string[] args)
+    {
+        MyClass c = new MyClass();
+        MyClass c1 = new MyClass();
+        MyClass c2 = new MyClass();
+        // MyClass.Counter = -100; если мы сделаем так, то нарушится инкапсуляция, мы не должный иметь возможность обновлять счетчик
+        // какимто образом вне класса, потому что мы можем поломать логику этого счетчика, именно в этом заключается задача свойств
+        Console.WriteLine(MyClass.Counter);
+    }
+}
