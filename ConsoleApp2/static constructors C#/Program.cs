e218053a027f0d187239da46515e3cdec037f439
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-26 15:23:04 +0500

    Добавил знание к статическим конструкторам, и добавил задачу к ним

---
+++ ConsoleApp2/static constructors C#/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Ключевое слово static
+ * Статические конструктор*/
+
+using System.Threading.Channels;
+
+class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine("Constuctos");
+    }
+
+    static MyClass()
+    {
+        Console.WriteLine("Static Constructots");
+    }
+
+    public static void Foo()
+    {
+        Console.WriteLine("Foo");
+    }
+}
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        //MyClass c1 = new MyClass();
+        MyClass.Foo();
+        new MyClass();
+        new MyClass();
+        new MyClass();
+    }
+}
---
+++ ConsoleApp2/static constructrs C#task/Program.cs
@@ -0,0 +1,32 @@
+﻿class DbRepository
+{
+    private static string connectionString = "local BD";
+
+    static DbRepository()
+    {
+        ConfigurationManager configurationManager = new ConfigurationManager();
+        connectionString = configurationManager.GetConnectionString();
+    }
+
+    public void GetData()
+    {
+        Console.WriteLine("using: " + connectionString);
+    }
+}
+
+class ConfigurationManager
+{
+    public string GetConnectionString()
+    {
+        return "local DB";
+    }
+}
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+        DbRepository dbrepository = new DbRepository();
+        dbrepository.GetData();
+    }
+}
