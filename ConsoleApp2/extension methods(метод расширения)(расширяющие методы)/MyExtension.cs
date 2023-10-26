39e0dc31ea11ec531224c7de40f005e8a1a63f71
Bahodur Tursunov <tursunovb18@gmail.com>
2023-10-26 22:59:26 +0500

    extension methods

---
+++ ConsoleApp2/extension methods(метод расширения)(расширяющие методы)/MyExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace extension_methods_метод_расширения__расширяющие_методы_
+{
+    static class MyExtension
+    {
+        public static void Print(this DateTime datetime)
+        {
+            Console.WriteLine(datetime);
+        }
+    }
+}
---
+++ ConsoleApp2/extension methods(метод расширения)(расширяющие методы)/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace extension_methods_метод_расширения__расширяющие_методы_
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            currentDateTime.Print();
+        }
+    }
+}
