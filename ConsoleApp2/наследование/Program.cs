b4db24bc5bd799e3be737ced615cb3c8fa001cb1
Bakha <tursunovb18@gmail.com>
2023-12-06 22:02:45 +0500

    add: Inheritance in C#

---
+++ ConsoleApp2/наследование/Program.cs
@@ -0,0 +1,37 @@
+﻿/*наследование в ООП*/
+
+using System.Runtime.ExceptionServices;
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public void PrintName()
+    {
+        Console.WriteLine($"Меня зовут {FirstName}");
+    }
+}
+/* если говорить о терминологии, то в данном случае класс Student является наследником класса
+ * Person, а класс Person является родительским(базовым классом) для класса Student*/
+class Student : Person
+{
+    public void Learn()
+    {
+        Console.WriteLine("I`m studying");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Student { FirstName = "Bakha", LastName = "Tursunov"};
+        PrintFullName( person );
+       
+    }
+    static void PrintFullName(Person person)
+    {
+        Console.WriteLine($"LastName: {person.LastName}\t FirstName: {person.FirstName}");
+    }
+}
