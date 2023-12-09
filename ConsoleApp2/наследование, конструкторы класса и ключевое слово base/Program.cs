d37ad3a33266f8d3e6d1be6e83b756c2db7c1bb8
Bakha <tursunovb18@gmail.com>
2023-12-09 12:52:15 +0500

    change: все конкретно выводится и нету неоднозначности в коде, но в следующем коммите, у нас в конструкторах класса могут быть неоднозначности, т.е. их названия могут быть одинаковыми, и есть следующее решение к этому

--- ConsoleApp2/наследование, конструкторы класса и ключевое слово base/Program.cs
+++ ConsoleApp2/наследование, конструкторы класса и ключевое слово base/Program.cs
@@ -8,28 +8,42 @@
 
 class Point2D : MyClass
 {
-    public Point2D()
+    public Point2D(int x, int y)
     {
+        X = x;
+        Y = y;
         Console.WriteLine("Point2D");
     }
     public int X { get; set; }
     public int Y { get; set; }
+
+    public void Print2D()
+    {
+        Console.WriteLine("X:\t" + X);
+        Console.WriteLine("Y:\t" + Y);
+    }
 }
 
 class Point3D : Point2D
 {
-    public Point3D()
+    public Point3D(int x, int y, int z) : base(x, y)
     {
+        Z = z;
         Console.WriteLine("Point3D");
     }
     public int Z { get; set; }
+    public void Print3D()
+    {
+        base.Print2D();
+        Console.WriteLine("Z:\t" + Z);
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        Point3D point3D = new Point3D();
-
+        Point3D point3D = new Point3D(3,6,8);
+        point3D.Print3D();
     }
 }
