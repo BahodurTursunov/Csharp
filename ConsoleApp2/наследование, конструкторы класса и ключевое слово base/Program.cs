aba215a46ef630651e45547b4e59f5984ddda4a5
Bakha <tursunovb18@gmail.com>
2023-12-09 11:31:46 +0500

    add: inheritance, constructors in class and base keyword

---
+++ ConsoleApp2/наследование, конструкторы класса и ключевое слово base/Program.cs
@@ -0,0 +1,35 @@
+﻿class MyClass
+{
+    public MyClass()
+    {
+        Console.WriteLine("MyClass");
+    }
+}
+
+class Point2D : MyClass
+{
+    public Point2D()
+    {
+        Console.WriteLine("Point2D");
+    }
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+class Point3D : Point2D
+{
+    public Point3D()
+    {
+        Console.WriteLine("Point3D");
+    }
+    public int Z { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point3D point3D = new Point3D();
+
+    }
+}
