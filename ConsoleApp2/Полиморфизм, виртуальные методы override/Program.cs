db4d76378413dc4316e83d10e816962d16e78b76
Bakha <tursunovb18@gmail.com>
2023-12-13 14:21:11 +0500

    add: polimorphism project

---
+++ ConsoleApp2/Полиморфизм, виртуальные методы override/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Полиморфизм 
+ * 
+ * виртуальные методы
+ * 
+ * virtual
+ * 
+ * override
+ * 
+ */
+
+class Car
+{
+    public void Drive()
+    {
+        Console.WriteLine("Я машина, я еду!");
+    }
+}
+
+class SportCar : Car
+{
+
+}
+class Person
+{
+    public void Drive(Car car)
+    {
+        car.Drive();
+    }
+
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Car car = new Car();
+
+        //Person person = new Person();
+
+        //person.Drive(car);
+
+        Person person = new Person();
+
+        person.Drive(new SportCar());
+    }
+}
