5ec7baccc289b94d2a7c9eae96c7324b145aa0a6
Bakha <tursunovb18@gmail.com>
2023-12-13 14:01:38 +0500

    add: project public private protected modificators

---
+++ ConsoleApp2/модификатор доступа protected при наследовании/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace модификатор_доступа_protected_при_наследовании
+{
+    class A
+    {
+        public int publicField;
+        private int privateField;
+        protected int protectedField;
+
+        public A()
+        {
+            Console.WriteLine(publicField);
+            Console.WriteLine(privateField);
+            Console.WriteLine(protectedField);
+        }
+    }
+
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine(publicField);
+            Console.WriteLine(privateField);
+            Console.WriteLine(protectedField);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            Console.WriteLine(a.publicField);
+            //Console.WriteLine(a.privateField);
+            //Console.WriteLine(a.protectedField);
+        }
+    }
+}
