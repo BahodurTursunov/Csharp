b67575e725410b1a53a60fef34d71b0390746866
Bakha <tursunovb18@gmail.com>
2023-12-13 13:27:03 +0500

    add: project приведение типов и использование операторов as and is

---
+++ ConsoleApp2/приведение типов и наследование (использование операторов as и is)/Program.cs
@@ -0,0 +1,41 @@
+﻿class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void Print()
+    {
+        Console.WriteLine("X:\t" + X);
+        Console.WriteLine("Y:\t" + Y);
+    }
+
+   
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        object obj = new Point { X = 3, Y = 5 };
+        //object obj = "hello";
+        Foo(obj);
+        Bar(obj);
+    }
+    static void Foo(object obj)
+    {
+        Point point = obj as Point;
+
+        if (point != null)
+        {
+            point.Print();
+        }
+    }
+
+    static void Bar(object obj)
+    {
+        if (obj is Point point)
+        {
+            point.Print();
+        }
+    }
+}
