97ac3f445e82892ed1503021d9de5e45a4151b5f
Bakha <tursunovb18@gmail.com>
2024-01-11 13:05:42 +0500

    add: интерфейсы и полиморфизм, здесь было тронуто как реализовывать интерфейсы

---
+++ ConsoleApp2/интерфейсы и полиморфизм/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace интерфейсы_и_полиморфизм
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataProcessor dataProcessor = new ConsoleDataProcessor();
+            dataProcessor.ProcessData(new DbDataProvider());
+            dataProcessor.ProcessData(new FileDataProvider());
+            dataProcessor.ProcessData(new APIDataProvider());
+        }
+    }
+
+    class DbDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "data from db";
+        }
+    }
+
+    class FileDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "data from file";
+        }
+    }
+
+    class APIDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "data from api";
+        }
+    }
+
+    class ConsoleDataProcessor : IDataProcessor
+    {
+        public void ProcessData(IDataProvider dataProvider)
+        {
+            Console.WriteLine(dataProvider.GetData());
+        }
+    }
+    interface IDataProvider
+    {
+        string GetData();
+    }
+
+    interface IDataProcessor
+    {
+        void ProcessData(IDataProvider dataProvider);
+    }
+}
