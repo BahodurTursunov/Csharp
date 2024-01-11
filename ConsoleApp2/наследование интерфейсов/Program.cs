277ee398feed6f4953663c1e9c0ea659cd722609
Bakha <tursunovb18@gmail.com>
2024-01-11 13:06:33 +0500

    add: наследование интерфейсов (здесь была тронута тема как наследоваться от интерфейсов и их реализация

---
+++ ConsoleApp2/наследование интерфейсов/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace наследование_интерфейсов
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            IWeapon[] inventory = { new Gun(), new LaserGun(), new Knife() };
+
+            foreach (var item in inventory)
+            {
+                player.Fire(item);
+                Console.WriteLine();
+            }
+            player.Throw(new Knife());
+        }
+
+    }
+
+    interface IWeapon
+    {
+        void Fire();
+    }
+
+    interface IThrowingWeapon : IWeapon
+    {
+        void Throw();
+    }
+
+    class Gun : IWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine($"{GetType().Name}: Пыщ");
+        }
+    }
+
+    class LaserGun : IWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine($"{GetType().Name}: Птю");
+        }
+    }
+
+    class Knife : IThrowingWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine($"{GetType().Name}: Хыщ");
+
+        }
+
+        public void Throw()
+        {
+            Console.WriteLine($"{GetType().Name}: Фють");
+        }
+    }
+
+    class Player
+    {
+        public void Fire(IWeapon weapon)
+        {
+            weapon.Fire();
+        }
+
+        public void Throw(IThrowingWeapon throwingWeapon)
+        {
+            throwingWeapon.Throw();
+        }
+    }
+}
